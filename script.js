b5d2ad2f9ef27e7435a540b021a4f0358fe94005
Sachin Kumar <96971711+kumarsachin70@users.noreply.github.com>
2023-04-08 15:52:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Set the details of the birthday event
+const date = new Date('2023-04-15');
+document.getElementById('date').textContent = date.toDateString();
+document.getElementById('time').textContent = '6:00 PM';
+document.getElementById('location').textContent = '123 Main St, Anytown, USA';
+document.getElementById('dress-code').textContent = 'Casual';
+
+// Handle
